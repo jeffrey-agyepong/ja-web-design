b2448000c126c9401b12c4016941b8c31b93ddd5
Jeffrey A <agyepong99@gmail.com>
2024-07-20 16:38:16 -0600

    pricing

--- src/navigation.js
+++ src/navigation.js
@@ -44,7 +44,7 @@ export const footerData = {
         { text: 'About', href: getPermalink('/about') },
         { text: 'Projects', href: getPermalink('/index.html#projects') },
         {
-          text:'Performance', href: getPermalink('/posts/performance')
+          text: 'Performance', href: getPermalink('/posts/performance')
         },
         { text: 'Services', href: getPermalink('/services') },
         { text: 'Contact', href: getPermalink('/contact') },
@@ -63,7 +63,12 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/jeffrey-agyepong' },
   ],
   footNote: `
-    <span class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5  rtl:float-right rounded-sm"></span>
-    © 2024 J.A Web Design· All rights reserved.
+    <div class="sm:flex gap-80 justify-center items-center">
+    <p>Developed by <a href="https://jawebdesign.ca/" class="text-red-400">J.A Web Design</a>
+    </p>
+     <p>
+    © 2024 J.A Web Design 
+    </p>
+    </div>
   `,
 };

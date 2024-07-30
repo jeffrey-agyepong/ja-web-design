897329d2969e6d424eac09776e126cc7144cee58
Jeffrey A <agyepong99@gmail.com>
2024-07-30 17:02:07 -0600

    Update navigation.js

--- src/navigation.js
+++ src/navigation.js
@@ -49,7 +49,7 @@ export const footerData = {
         { text: 'Services', href: getPermalink('/services') },
         { text: 'Contact', href: getPermalink('/contact') },
         { text: 'Pricing', href: getPermalink('/pricing') },
-        { text: 'Referral Program', href: getPermalink('/referral-program') }
+        // { text: 'Referral Program', href: getPermalink('/referral-program') }
       ],
     },
 
